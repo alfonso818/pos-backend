a4b24d6dd407d9ebb57cb69733654ce9272614c1
alfonso818 <alfonsoguillera28@gmail.com>
2026-05-15 20:45:18 +0700

    pos-backend first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('POS Backend Running');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Server Running');
+});
